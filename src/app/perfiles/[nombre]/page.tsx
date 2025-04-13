1791d6a88620107e0f2ef7ecf5184e7ae6d9c6ff
Alison Puerta <yisethvelez07@gmail.com>
2025-04-12 20:33:11 -0500

    Eliminamos los imports no usados

--- src/app/perfiles/[nombre]/page.tsx
+++ src/app/perfiles/[nombre]/page.tsx
@@ -7,15 +7,9 @@ import Header from "@/app/components/header";
 import { TbZodiacCancer } from "react-icons/tb";
 import { TbZodiacScorpio } from "react-icons/tb";
 import { TbZodiacTaurus } from "react-icons/tb";
-import { TbZodiacCapricorn } from "react-icons/tb";
-import { TbZodiacPisces } from "react-icons/tb";
 import { TbZodiacAquarius } from "react-icons/tb";
 import { TbZodiacAries } from "react-icons/tb";
-import { TbZodiacGemini } from "react-icons/tb";
 import { TbZodiacLeo } from "react-icons/tb";
-import { TbZodiacVirgo } from "react-icons/tb";
-import { TbZodiacLibra } from "react-icons/tb";
-import { TbZodiacSagittarius } from "react-icons/tb";
 
 export default function Perfil() {
   const params = useParams(); // Usa useParams para obtener los parámetros de la URL
